3e805b09ba54eb74b388e6fb148eb4dbd8d2763c
Thomas Jonstrup <thomas_jonstrup@hotmail.com>
2024-04-13 18:20:37 +0200

    Initial commit

---
+++ .svelte-kit/generated/client/app.js
@@ -0,0 +1,25 @@
+export { matchers } from './matchers.js';
+
+export const nodes = [
+	() => import('./nodes/0'),
+	() => import('./nodes/1'),
+	() => import('./nodes/2'),
+	() => import('./nodes/3'),
+	() => import('./nodes/4')
+];
+
+export const server_loads = [0];
+
+export const dictionary = {
+		"/slice-simulator": [2],
+		"/[[preview=preview]]": [~3],
+		"/[[preview=preview]]/[uid]": [~4]
+	};
+
+export const hooks = {
+	handleError: (({ error }) => { console.error(error) }),
+
+	reroute: (() => {})
+};
+
+export { default as root } from '../root.svelte';
